e2013e63386e6cae3c25d096901a6e5876229c7d
SaikouTrawally <B00161930@mytudublin.ie>
2025-02-10 18:19:02 +0000

    Completed Part B

---
+++ WK2PartB.py
@@ -0,0 +1,15 @@
+#Saikou Trawally
+#10-02-2025
+#Part B
+
+def printSteps (a,b):
+    for i in range(a, b+1):
+        print(f"Step{i}")
+
+def main():
+    a = 1
+    b = 9
+    printSteps(a,b)
+
+
+main()    
---
+++ Wk2PartC.py
@@ -0,0 +1,15 @@
+#Saikou Trawally
+#10-02-2025
+#Part B
+
+def printSteps (a,b):
+    for i in range(a, b+1):
+        print(f"Step{i}")
+
+def main():
+    a = int(input("Enter Step")
+    b = int(input("Enter Step")
+    printSteps(a,b)
+
+
+main()    
